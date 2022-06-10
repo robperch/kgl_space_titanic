d9b3f1645bc6c40479f059237a1faae41515d6d0
Roberto919 <robertosysm@gmail.com>
2022-06-09 19:41:48 -0500

    creating first version of parameters

--- pkg_dir/src/parameters/titanicsp_params.py
+++ pkg_dir/src/parameters/titanicsp_params.py
@@ -21,6 +21,130 @@
 
 
 
+"----------------------------------------------------------------------------------------------------------------------"
+############### Data schema ############################################################################################
+"----------------------------------------------------------------------------------------------------------------------"
+
+
+## Data schema to handle data
+titanicsp_data_schema = {
+
+    'PassengerId': {
+        'relevant': True,
+        'clean_col_name': 'PassengerId',
+        'data_type': 'str',
+        'feature_type': 'categorical',
+        'model_relevant': False,
+        'id_feature': True,
+    },
+
+    'HomePlanet': {
+        'relevant': True,
+        'clean_col_name': 'HomePlanet',
+        'data_type': 'str',
+        'feature_type': 'categorical',
+        'model_relevant': True,
+    },
+
+    'CryoSleep': {
+        'relevant': True,
+        'clean_col_name': 'CryoSleep',
+        'data_type': 'bool',
+        'value_map': {
+            'False': False,
+            'True': True,
+        },
+        'feature_type': 'boolean',
+        'model_relevant': True,
+    },
+
+    'Cabin': {
+        'relevant': True,
+        'clean_col_name': 'Cabin',
+        'data_type': 'str',
+        'feature_type': 'categorical',
+        'model_relevant': True,
+    },
+
+    'Destination': {
+        'relevant': True,
+        'clean_col_name': 'Destination',
+        'data_type': 'str',
+        'feature_type': 'categorical',
+        'model_relevant': True,
+    },
+
+    'Age': {
+        'relevant': True,
+        'clean_col_name': 'Age',
+        'data_type': 'int',
+        'feature_type': 'numerical',
+        'model_relevant': True,
+    },
+
+    'VIP': {
+        'relevant': True,
+        'clean_col_name': 'VIP',
+        'data_type': 'bool',
+        'value_map': {
+            'False': False,
+            'True': True,
+        },
+        'feature_type': 'boolean',
+        'model_relevant': True,
+    },
+
+    'RoomService': {
+        'relevant': True,
+        'clean_col_name': 'RoomService',
+        'data_type': 'float',
+        'feature_type': 'numerical',
+        'model_relevant': True,
+    },
+
+    'FoodCourt': {
+        'relevant': True,
+        'clean_col_name': 'FoodCourt',
+        'data_type': 'float',
+        'feature_type': 'numerical',
+        'model_relevant': True,
+    },
+
+    'ShoppingMall': {
+        'relevant': True,
+        'clean_col_name': 'ShoppingMall',
+        'data_type': 'float',
+        'feature_type': 'numerical',
+        'model_relevant': True,
+    },
+
+    'Spa': {
+        'relevant': True,
+        'clean_col_name': 'Spa',
+        'data_type': 'float',
+        'feature_type': 'numerical',
+        'model_relevant': True,
+    },
+
+    'Name': {
+        'relevant': True,
+        'clean_col_name': 'Name',
+        'data_type': 'str',
+        'feature_type': 'categorical',
+        'model_relevant': True,
+    },
+
+    'Transported': {
+        'relevant': True,
+        'clean_col_name': 'Transported',
+        'predict_label': True,
+    },
+
+}
+
+
+
+
 
 "----------------------------------------------------------------------------------------------------------------------"
 ############### XXX ####################################################################################
