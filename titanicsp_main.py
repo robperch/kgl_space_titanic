a4adb35b41c6f4d9c0efcf97727120f1fa38cb9e
Roberto919 <robertosysm@gmail.com>
2022-07-05 15:36:17 -0500

    adjustments to test transform

--- titanicsp_main.py
+++ titanicsp_main.py
@@ -38,17 +38,17 @@ def pipeline_main_func():
     """
 
 
-    ## Extract pipeline function
-    extract_pipeline_func()
-
+    # ## Extract pipeline function
+    # extract_pipeline_func()
+    #
     ## Transform pipeline function
     transform_pipeline_func()
-
-    ## Feature engineering pipeline function
-    feateng_pipeline_func()
-
-    ## Models training pipeline function
-    modtrain_pipeline_func()
+    #
+    # ## Feature engineering pipeline function
+    # feateng_pipeline_func()
+    #
+    # ## Models training pipeline function
+    # modtrain_pipeline_func()
 
 
     return
