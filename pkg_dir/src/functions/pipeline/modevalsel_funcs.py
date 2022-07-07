53da993bbaca17958564383b2b9eb0c115600812
Roberto919 <robertosysm@gmail.com>
2022-07-07 17:04:29 -0500

    creating initial section of modtrain funcs

--- pkg_dir/src/functions/pipeline/modevalsel_funcs.py
+++ pkg_dir/src/functions/pipeline/modevalsel_funcs.py
@@ -37,7 +37,24 @@ from pkg_dir.src.parameters import *
 
 "--------------- Compounded functions ---------------"
 
-## Model selection pipeline function
+## Models evaluation and selection pipeline function
+def modevalsel_pipeline_func():
+    """
+    Models evaluation and selection pipeline function
+
+    :return:
+    """
+
+    ## Saving dataset objects from the 'feateng' step of the pipeline in a dictionary data structure
+    dataset_dict = dataset_objects_dict(pipeline_pkl_modtrain_local_dir)
+
+    ## Magic loop: iterating over various models and hyper-parameters to find best parameters
+
+    # ## Saving module results
+    # save_modtrain_results(dataset_dict, models_magic_loop)
+
+
+    return
 
 
 
